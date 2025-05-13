429ccc063f927f3fa2afb13fcbd2c20af0aa08b8
kevin cuadros <kevindavidcuadros@gmail.com>
2025-05-12 21:09:49 -0500

    Initial Commit

---
+++ GroceriesShop/GroceriesShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesShopApp.swift
+//  GroceriesShop
+//
+//  Created by Kevin Cuadros on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
