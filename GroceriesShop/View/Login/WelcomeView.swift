7d90aba57147791d0042d141627ab7d4cd35ea89
kevin cuadros <kevindavidcuadros@gmail.com>
2025-05-13 19:24:32 -0500

    add fonts, create common extension(Color, Font)

---
+++ GroceriesShop/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  GroceriesShop
+//
+//  Created by Kevin Cuadros on 13/05/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
