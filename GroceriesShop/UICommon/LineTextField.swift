85d90818c2200d72e10fda2d109a42a88fe144b1
kevin cuadros <kevindavidcuadros@gmail.com>
2025-05-20 20:31:58 -0500

    update login screen, create extension

--- GroceriesShop/UICommon/LineTextField.swift
+++ GroceriesShop/UICommon/LineTextField.swift
@@ -12,6 +12,8 @@ struct LineTextField: View {
     @Binding var text: String
     @State var title: String = "Title"
     @State var placeholder: String = "Placeholder"
+    @State var keyboardType: UIKeyboardType = .default
+    @State var isPassword: Bool = false
     
     var body: some View {
         VStack {
@@ -20,8 +22,41 @@ struct LineTextField: View {
                 .foregroundColor(.textTitle)
                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             
-            TextField(placeholder, text: $text)
+            if isPassword {
+                SecureField(placeholder, text: $text)
+            } else {
+                TextField(placeholder, text: $text)
+                    .keyboardType(keyboardType)
+            }
+            Divider()
+        }
+    }
+}
+
+struct LineSecureField: View {
+    
+    @Binding var text: String
+    @Binding var isShowPassword: Bool
+    
+    @State var title: String = "Title"
+    @State var placeholder: String = "Placeholder"
+    
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.customFont(.semibold, fontSize: 16))
+                .foregroundColor(.textTitle)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             
+            if isShowPassword {
+                TextField(placeholder, text: $text)
+                    .modifier(ShowButton(isShow: $isShowPassword))
+                
+            } else {
+                SecureField(placeholder, text: $text)
+                    .modifier(ShowButton(isShow: $isShowPassword))
+            }
             Divider()
         }
     }
@@ -29,4 +64,5 @@ struct LineTextField: View {
 
 #Preview {
     LineTextField(text: .constant(""))
+    LineSecureField(text: .constant(""), isShowPassword: .constant(true))
 }
--- GroceriesShop/View/Login/LoginView.swift
+++ GroceriesShop/View/Login/LoginView.swift
@@ -19,125 +19,100 @@ struct LoginView: View {
                 .scaledToFill()
                 .frame(width: .screenWidth, height: .screenHeight)
             
+            VStack{
+                
+                Image(.colorLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40)
+                    .padding(.bottom, .screenWidth * 0.1)
+                
+                
+                Text("Login")
+                    .font(.customFont(.semibold, fontSize: 26))
+                    .foregroundColor(.primaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 4)
+                
+                Text("Enter your emails and password")
+                    .font(.customFont(.semibold, fontSize: 16))
+                    .foregroundColor(.secondaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, .screenWidth * 0.1)
+                
+                LineTextField(
+                    text: $loginVM.textEmail,
+                    title: "Email",
+                    placeholder: "Enter your email address",
+                    keyboardType: .emailAddress
+                )
+                    .padding(.bottom, .screenWidth * 0.07)
+                
+                
+                LineSecureField(
+                    text: $loginVM.textPassword,
+                    isShowPassword: $loginVM.isShowPassword, title: "Password",
+                    placeholder: "Enter your password"
+                )
+                    .padding(.bottom, .screenWidth * 0.02)
+                
+                
+                NavigationLink {
+                    // Forgot Password
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.customFont(.medium, fontSize: 14))
+                        .foregroundColor(.primaryText)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                .padding(.bottom, .screenWidth * 0.03)
+                
+                RoundedButton(title: "Log In") {
+                }
+                .padding(.bottom, .screenWidth * 0.05)
+                
+                
+                NavigationLink {
+                    // Sigup
+                } label: {
+                    HStack{
+                        Text("Don’t have an account?")
+                            .font(.customFont(.semibold, fontSize: 14))
+                            .foregroundColor(.primaryText)
+                        
+                        Text("Signup")
+                            .font(.customFont(.semibold, fontSize: 14))
+                            .foregroundColor(.primary)
+                    }
+                }
+                
+                
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets + 64)
+            .padding(.horizontal, 20)
+            .padding(.bottom, .bottomInsets)
+            
             VStack {
                 HStack {
                     Button {
                         dismiss()
                     } label: {
-                        Image(.back)
+                        Image("back")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 20, height: 20)
                     }
+                    
                     Spacer()
-                }
-                .padding(.top, .topInsets )
-                .padding(.bottom, .bottomInsets)
-                
-                Image(.colorLogo)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 60, height: 60)
-                    .padding(.bottom, 60)
-                
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 15) {
-                        Text("Login")
-                        
-                            .font(.customFont(.semibold, fontSize: 26))
-                        Text("Enter your emails and password")
-                            .font(.customFont(.medium, fontSize: 16))
-                            .foregroundColor(.textTitle)
-                    }
-                    Spacer()
-                }
-                .padding(.bottom, 40)
-                
-                VStack(alignment: .leading) {
-                    Text("Email")
-                        .font(.customFont(.semibold, fontSize: 16))
-                        .foregroundColor(.textTitle)
-                        .padding(.bottom, 5)
-                    
-                    TextField("Enter your email", text: $loginVM.textEmail)
-                        .keyboardType(.emailAddress)
-                        .padding(.bottom, 5)
-                    
-                    Divider()
-                        .padding(.bottom, 40)
-                    
-                    
-                    VStack(alignment: .leading) {
-                        Text("Password")
-                            .font(.customFont(.semibold, fontSize: 16))
-                            .foregroundColor(.textTitle)
-                            .padding(.bottom, 5)
-                        
-                        HStack {
-                            Group {
-                                if loginVM.isShowPassword {
-                                    TextField(
-                                        "Enter your password",
-                                        text: $loginVM.textPassword
-                                    )
-                                    .padding(.bottom, 5)
-                                    .frame(height: 30)
-                                    
-                                } else {
-                                    SecureField(
-                                        "Enter your password",
-                                        text: $loginVM.textPassword
-                                    )
-                                    .textContentType(.password)
-                                    .padding(.bottom, 5)
-                                    .frame(height: 30)
-                                }
-                            }.animation(.easeInOut(duration: 0.2), value: loginVM.isShowPassword)
-                            
-                            Button {
-                                loginVM.isShowPassword.toggle()
-                            } label: {
-                                Image(
-                                    systemName: loginVM.isShowPassword ? "eye.slash.fill" : "eye.fill"
-                                )
-                                .frame(width: 20, height: 20)
-                                .foregroundStyle(.gray)
-                            }
-                        }
-                    }
-                    Divider()
-                        .padding(.bottom, 10)
-                                        
-                    HStack {
-                        Spacer()
-                        Text("Forgot Password?")
-                            .font(.customFont(.medium, fontSize: 15))
-                            .foregroundColor(.black.opacity(0.8))
-                    }
-                    .padding(.bottom, 30)
-                    
-                    RoundedButton(title: "Log in") {
-                        
-                    }
-                    .padding(.bottom, 30)
-                    
-                    HStack(alignment: .center) {
-                        Spacer()
-                        Text("Don’t have an account?")
-                            .font(.customFont(.semibold, fontSize: 15))
-                            .foregroundColor(.black)
-                        Text("Singup")
-                            .font(.customFont(.semibold, fontSize: 15))
-                            .foregroundColor(Color.primary)
-                        Spacer()
-                    }
-                    
                 }
                 Spacer()
+                
             }
+            .padding(.top, .topInsets)
             .padding(.horizontal, 20)
-            .padding(.bottom, 40)
+            
             
         }
         .navigationTitle("")
