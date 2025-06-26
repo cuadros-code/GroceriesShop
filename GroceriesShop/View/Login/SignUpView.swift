7c6ec72cc22a35e851d1d16f9be92c8e015b4cbb
kevin cuadros <kevindavidcuadros@gmail.com>
2025-06-26 17:10:55 -0500

    create SignUp view

---
+++ GroceriesShop/View/Login/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  GroceriesShop
+//
+//  Created by Kevin Cuadros on 26/06/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
